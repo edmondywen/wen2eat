aa7d0d38ef48a7c9eefab6eb6f7552e52de2550e
Jess Xu <jsxu315@gmail.com>
2021-11-07 01:22:21 -0800

    Added basic firebase read/write
    Added a button to push an ingredient to the remote database,
    and a button to print the contents of the database to console.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+//Note: move this to firebase.js but I'm too scared to break something rn 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA22TwxWMJI6G9MqXG28aRiGZkXcluNXco",
+  authDomain: "wen2eat.firebaseapp.com",
+  projectId: "wen2eat",
+  storageBucket: "wen2eat.appspot.com",
+  messagingSenderId: "718202186206",
+  appId: "1:718202186206:web:bdc477289c0f8a2242154f",
+  measurementId: "G-QFS90PYCG8"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
+
+
+const db = firebase.firestore();
+
+function makeIngredient(name, expirationdate) {
+  return (
+    <div class = "ingredient">
+      <p>Ingredient: {name} </p>
+      <p>Expires in: {expirationdate}</p>
+      <button > 
+          Delete ingredient (this button does nothing)
+        </button> 
+    </div>
+  ); 
+}
+
+function addBread() {
+  db.collection("pantry").add({
+    ingredient: "bread",
+    expirationdate: "10 days"
+  })
+  .then((docRef) => {
+    console.log("Document written with ID: ", docRef.id);
+  })
+  .catch((error) => {
+      console.error("Error adding document: ", error);
+  });
+}
+
+function printDatabase(){
+  db.collection("pantry").get()
+      //snapshot is the status of the collection at the current time 
+    //docs.map.(etc) gets an array of the documents in the database 
+  .then((snapshot) => {console.log(snapshot.docs.map((doc) => doc.data()))}) 
+  .catch((error) => console.error("Error getting documents: ", error));
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {makeIngredient("apples", "2 days")}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <button onClick = {() =>addBread()}>Add bread to database</button>
+        
+        <button onClick = {() =>printDatabase()}>Print database contents to console</button>
+
+        
       </header>
+
+      
     </div>
   );
 }
