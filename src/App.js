8db6752d8c362bc60113e71f205a9d1d6f714cbf
Cameron Fiske <cameronfiske@Camerons-MacBook-Pro.local>
2021-11-09 18:39:32 -0800

    spoonAPI connected Now you can get recepie recommondatiosn for what you have added to the pantry. gives 5 and cannot clear without refresh no query options yet.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
+//Spoonacular API Key: f87bfe3073584580bd8a6fb6eafa20f8
+
 import IngredientForm from "./components/IngredientForm"
 import { useEffect, useState } from "react"
-import { onSnapshot, collection, setDoc, doc, addDoc, deleteDoc, Timestamp } from "@firebase/firestore"
+import { onSnapshot, collection, setDoc, doc, addDoc, getDoc, deleteDoc, Timestamp } from "@firebase/firestore"
 import db from './firebase'
+import RecepieCard from "./components/RecepieCard"
 
 function App() {
   const [ingredients, setIngredients] = useState([])
-
-  const setupFirestoreListener = () => {
-    console.log(db);
-    return onSnapshot(collection(db, "ingredients"), (snapshot) =>
-    setIngredients(snapshot.docs.map((doc) => ({ingredient: doc.data().ingredient, expiration: doc.data().expiration.toDate(), id: doc.id}) )
-    ))
-  }
-  useEffect(setupFirestoreListener, []);
-  
 
   const [next, setNext] = useState(
     {
@@ -21,6 +15,20 @@ function App() {
       expiration: "",
     }
   )
+
+  const [recepie, setRecepie] = useState([])
+
+  const setupFirestoreListener = () => {
+    console.log(db);
+    // getRecepie();
+    return onSnapshot(collection(db, "ingredients"), (snapshot) => 
+      setIngredients(snapshot.docs.map((doc) => (
+        {ingredient: doc.data().ingredient, expiration: (doc.data().expiration === undefined) ? "" : doc.data().expiration.toDate(), id: doc.id}
+        ))
+    ))
+  }
+  useEffect(setupFirestoreListener, []);
+  
 
   function handleIngChange(event){
     setNext({...next, ingredient: event.target.value})
@@ -39,6 +47,10 @@ function App() {
   async function addIngredient(ingredient)
   {
     const collectionRef = collection(db, 'ingredients')
+    if(!(ingredients.expiration instanceof Date))
+    {
+      delete ingredient.expiration
+    }
     await addDoc(collectionRef, ingredient)
   }
 
@@ -52,17 +64,44 @@ function App() {
     )
   }
 
+  function getRecepie()
+  {
+    let ingredientString = ""
+    ingredients.forEach((ingredient) => ingredientString = ingredientString + "," +ingredient.ingredient)
+    console.log(ingredientString)
+    fetch(
+      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=f87bfe3073584580bd8a6fb6eafa20f8&number=5&ingredients=${ingredientString}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setRecepie(data)
+        console.log(recepie)
+      })
+      .catch(() => {
+        console.log("error");
+      });
+      // recepie.forEach((rec) => console.log(rec.title))
+  }
+
   return (
     <div className="body">
-      <h1>Wen2Eat </h1>
+      <h1>Wen2Eat</h1>
       <IngredientForm onSubmit={handleSubmit} ingChange={handleIngChange} expChange={handleExpChange} current_expiration={next.expiration} current_ingredient={next.ingredient}/>
       <button onClick={clearAll}>Clear</button>
+      <button onClick={getRecepie}>wat to eat</button>
+
       {ingredients.map((entry) => (
         <div>
           <h5>{entry.ingredient}</h5>
           <h6>{entry.expiration.toString()}</h6>
         </div>
       ))}
+
+      {recepie.map((entry) => (
+        <div><RecepieCard recepie={entry}/></div>
+      ))}
+      
     </div>
   );
 }
