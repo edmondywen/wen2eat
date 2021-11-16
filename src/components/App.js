177470c78a4a84894e1a41d8a85e21c37438cac7
edmondywen <edmondywen@gmail.com>
2021-11-15 22:09:15 -0800

    added expiration date field for ingredients list. TODO: make the ingredients section prettier

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-        </p>
         <Pantry></Pantry>
       </header>
     </div>
--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -5,6 +5,8 @@ Ingredient
 the individual ingredient element. 
 
 props: 
+- itemName: the name of the ingredient itself
+- expDate: the expiration date for the ingredient
 
 states:
 - isFavorite: boolean that indicates if the ingredient should be prioritized during recipe search 
@@ -14,6 +16,7 @@ function Ingredient(props) {
     return (
         <div className="Ingredient">
             <p>{props.itemName}</p>
+            <p>{props.expDate}</p>
             {/* <input type="checkbox"/>
             <button>x</button> */}
         </div>
