7ba6502fea055ec01269f383ac1f4cbc4a8465ce
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2021-11-21 15:18:54 -0800

    folder w/ dietary restrictions stuff (basically modified pantry)

---
+++ src/componentsDR/Application.js
@@ -0,0 +1,16 @@
+import DRList from './DRList.js'
+import './Application.css';
+
+function Application() {
+  return (
+    <div className="Application">
+      <header className="App-header">
+        <p>
+        </p>
+        <DRList></DRList>
+      </header>
+    </div>
+  );
+}
+
+export default Application;
