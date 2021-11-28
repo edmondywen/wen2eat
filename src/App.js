04b6c044d55ec3d2f0be985f15249522195867d0
Cameron Fiske <cameronfiske@Camerons-MacBook-Pro.local>
2021-11-28 13:27:52 -0800

    Sorting of items by expiration implemented
    This is done via a button but can easily be
    integrated into the fetching process.
    Additionally, 2 more Spoonacular accounts
    were added to decreatse the likelyhood
    of running out of daily API points

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import RecipeCard from "./components/RecipeCard"
 
 
 function App() {
+  const APIKEY = ['f5c4a28754c8421a87b7caae4e66f5b8', 'f87bfe3073584580bd8a6fb6eafa20f8', '172c8e43ebeb4f848f87dae833c0165d']
+  const RECIPES_PER_PAGE = 10
   const [ingredients, setIngredients] = useState([])
   const [next, setNext] = useState(
     {
@@ -103,9 +105,11 @@ function App() {
     return ingredients; 
   }
 
-  function getRecipe()
+  async function getRecipe(key)
   {
     let ingredientString = ""
+    console.log("1")
+    console.log(ingredients)
     //TODO
     // escape spaces and special chars in ingredient names
     ingredients.forEach((ingredient) => ingredientString = ingredientString + "," +ingredient.ingredient)
@@ -114,36 +118,95 @@ function App() {
       /* TODO
       make the spoonacular quiery depend on quantity and expiration*/
       // `https://api.spoonacular.com/recipes/findByIngredients?apiKey=f87bfe3073584580bd8a6fb6eafa20f8&number=5&ingredients=${ingredientString}`
-        `https://api.spoonacular.com/recipes/complexSearch?apiKey=f87bfe3073584580bd8a6fb6eafa20f8&includeIngredients=${ingredientString}&sort=min-missing-ingredients&addRecipeInformation=true&fillIngredients=true`
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${APIKEY[key]}&includeIngredients=${ingredientString}&sort=min-missing-ingredients&addRecipeInformation=true&fillIngredients=true`
         )
       .then((response) => response.json())
       .then((data) => {
         console.log(data.results)
         setRecipe(data.results)
-        console.log(recipe)
+        // console.log(recipe)
       })
       .catch(() => {
-        console.log("error");
+        if(key = APIKEY.length-1)
+        {
+          console.log("error")
+        }else
+        {
+          getRecipe(key+1)
+        }
       });
+      console.log("2")
+    console.log(recipe)
       // recipe.forEach((rec) => console.log(rec.title))
   }
 
-  // sortRecipes(unorderedRecipes)
-  // {
-  //   ingredient_weights = {}
-  //   current_date = Date.now.getTime()
-  //   ingredients.forEach((ing) => {
-  //     daysUntilExp = (ing.expiration.getTime() - current_date) / (1000*3600*24)
-  //     score = (daysUntilExp >= 1) ? 1/Math.log(daysUntilExp+.5) : 0
-  //     ingredient_weights[ing.ingredient] = score
-  //   })
-  //   orderedRecipes = []
-  //   unorderedRecipes.forEach((recipe) => {
-  //     score
-      
-  //   })
-  //   setRecipe(orderedRecipes)
-  // }
+  function getIngredientScore()
+  {
+    const ingredient_weights = {}
+    const current_date = Date.now()
+    ingredients.forEach((ing) => {
+      let daysUntilExp = 180
+      if(ing.expiration !== "")
+      {
+        daysUntilExp = (ing.expiration.getTime() - current_date) / (1000*3600*24)
+      }
+      const score = (daysUntilExp >= 1) ? 1/Math.log(daysUntilExp+.5) : 0
+      ingredient_weights[ing.ingredient] = score
+    })
+    return ingredient_weights
+  }
+
+  function insertRecipes(rec, sorted)
+  {
+    const half = parseInt(sorted.length/2)
+    if(sorted.length === 0)
+    {
+      sorted.push(rec)
+      return sorted
+    }else if(sorted.length === 1)
+    {
+      if(parseFloat(rec.score) > parseFloat(sorted[0].score))
+      {
+        sorted.push(rec)
+        return sorted
+      }else
+      {
+        let recArray = [rec]
+        return recArray.concat(sorted)
+      }
+    }else if(parseFloat(rec.score) > parseFloat(sorted[half].score))
+    {
+      // alert("called gt")
+      return sorted.slice(0,half).concat(insertRecipes(rec, sorted.slice(half)))
+    }else
+    {
+      return insertRecipes(rec, sorted.slice(0, half)).concat(sorted.slice(half))
+    }
+  }
+
+  function sortRecipes(unorderedRecipes)
+  {
+    const ingredient_weights = getIngredientScore()
+    let orderedRecipeEncodings = []
+    unorderedRecipes.forEach((recipe, index) => {
+      const recepie_incoding = {position: index, score: 0}
+      ingredients.forEach((in_pantry) => {
+        recipe.extendedIngredients.forEach((in_recepie) => {
+          if(in_recepie.name.includes(in_pantry.ingredient))
+          {
+            recepie_incoding["score"] += ingredient_weights[in_pantry.ingredient]
+          }
+        })
+      })
+      orderedRecipeEncodings = insertRecipes(recepie_incoding, orderedRecipeEncodings)
+    })
+    let orderedRecipes = RECIPES_PER_PAGE < orderedRecipeEncodings.length ? new Array(RECIPES_PER_PAGE) : new Array(orderedRecipeEncodings.length)
+    for(let i = 0; i < RECIPES_PER_PAGE && i < orderedRecipeEncodings.length; i++)
+    {
+      orderedRecipes[i] = unorderedRecipes[orderedRecipeEncodings[i]["position"]]
+    }
+    setRecipe(orderedRecipes)
+  }
 
   return (
     <div className="body">
@@ -153,20 +216,18 @@ function App() {
 
       {/* <button onClick={() => getDate(doc(db, "ingredients", "someid"))}>get date </button>  */}
       <button onClick={() => getSortedIngredients()}>Order database by expiration date</button> 
-      <button onClick={getRecipe}>wat to eat</button>
+      <button onClick={() => (getRecipe(0))}>wat to eat</button>
       <button onClick={()=>(setRecipe([]))}>clear recipes</button>
+      <button onClick={()=>(sortRecipes(recipe))}>sort recipes</button>
+      {ingredients.length > 0 && ingredients.map((entry) => (
+          <div>
+            <h5>{entry.ingredient}</h5>
+            <h6>{entry.expiration.toLocaleString().substring(0,10)}</h6>
+          </div>
+        ))
+      }
 
-      {ingredients.map((entry) => (
-        //TODO
-        /*make ingredients display function. Make it so that 
-        expiration displays properly*/
-        <div>
-          <h5>{entry.ingredient}</h5>
-          <h6>{entry.expiration.toLocaleString().substring(0,10)}</h6>
-        </div>
-      ))}
-
-      {recipe.map((entry) => (
+      {recipe && recipe.map((entry) => (
         <div><RecipeCard recipe={entry}/></div>
       ))}
       
