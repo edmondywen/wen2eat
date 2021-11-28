425d350dd70c021ab454d945dcfb0a95646d900d
Jess Xu <jsxu315@gmail.com>
2021-11-27 20:36:16 -0800

    Authenticate logins using the firestore database
    On form submit, check the entered username/password against all the usernames/passwords
    in the database, and direct user to success/failure pages appropriately

--- src/App.js
+++ src/App.js
@@ -6,112 +6,56 @@ import LoginFail from "./components/LoginFail"
 import React, { useEffect, useState } from "react"
 import { BrowserRouter as Router, Route, Switch, Redirect, Link} from "react-router-dom"
 import db from './firebase'
-import {collection, getDocs} from "@firebase/firestore"
+import {collection, getDocs, onSnapshot} from "@firebase/firestore"
 
 const sampleuser = "forgetfulperson"; //Hardcoded username/password for testing purposes
 const samplepassword = "passwd"; 
+//let authenticating = false; 
+
+
 
 async function getQuerySnapshot()
 {
   //Search the collection "users" for a document with matching username and password 
   const querySnapshot = await getDocs(collection(db, "users"));  
-  querySnapshot.forEach((doc) => {
-    // doc.data() is never undefined for query doc snapshots
-    console.log(doc.id, " => ", doc.data());
-  });
+  console.log("getQuerySnapshot was called")
+  // querySnapshot.forEach((doc) => {
+  //   // doc.data() is never undefined for query doc snapshots
+  //   console.log(doc.id, " => ", doc.data());
+  // });
   return querySnapshot; 
 }
 
-// async function getQueryObjects()
-// {
-//   let users = []; 
-//   const querySnapshot = await getDocs(collection(db, "users"));
-//   querySnapshot.forEach((doc) => {
-//     // doc.data() is never undefined for query doc snapshots
-//     console.log(doc.id, " => ", doc.data());
-//     let user = { 
-//       username: doc.data().username, 
-//       password: doc.data().password
-//     }
-//     users.push(user); 
-//     console.log("Pushed " + user.username + " " + user.password); 
-//   });
-
-//   console.log(users); 
-
-//   return users; 
-// }
-
-function renderAuthenticate(username, password, submittedForm)
+function renderLoginResult(username, password, submittedForm, users)
 {
-  console.log("rendering authenticate"); 
+  console.log("rendering LoginResult"); 
 
-  if(submittedForm)
-  {
-    console.log("authenticate sees form submit")
-  }
-  else
-    console.log("authenticate does not see form submit")
-
-  return (<Authenticate username = {username} password = {password} submittedForm = {submittedForm} ></Authenticate>); 
+  return (<LoginResult username = {username} password = {password} submittedForm = {submittedForm} users = {users} ></LoginResult>); 
 }
 
-function Authenticate(props)
+
+function LoginResult(props)
 {
-  // console.log("passed in username as " + props.username); 
-  // console.log("passed in password as " + props.password); 
+  if(!props.submittedForm) 
+    return null; //render nothing because we're still on the login form page 
 
-  if(!props.submittedForm)
-    return (<p>no submit form yet</p>); //this also gets rendered on LoginFail because we reset submittedForm to false; need to fix that 
-
-    //NOTE: doesn't recognize valid username/passwords; prints out both if/else blocks in the same millisecond???? 
-  const querySnapshot = getQuerySnapshot(); 
-  // console.log("Type is " + typeof(querySnapshot)); 
-  // console.log(querySnapshot)
-  let counter = 0; 
-  let len = 0; 
-  querySnapshot.then(
-     querySnapshot => {
-       len = querySnapshot.size; 
-       console.log("querySnapshot size is " + querySnapshot.size); 
-
-       querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        //console.log("size is " + querySnapshot.size)
-        //console.log("Looking for " + props.username + " and " + props.password); 
-        if(doc.data().username == props.username && doc.data().password == props.password)
-        {
-          console.log("Found a match at " + Date.now())
-          return(<Redirect to = "/LoginSuccess"></Redirect>); 
-        }
-        else 
-        {
-          console.log("havent found a match for username " + props.username + " and password " + props.password + " at " + Date.now())
-        }
-        counter++; 
-      });
-
-    }
-  )
-  .catch(
-    (error) => console.log(error)
-  ) 
-
-  if(counter == len)
+  //Try to find a match for users 
+  let len = props.users.length
+  let i = 0; 
+  for( ; i < len; i++)
   {
-    console.log("wrong username/password"); 
-    return(<Redirect to = "/LoginFail"></Redirect>);
+    if(props.username == props.users[i].username && props.password == props.users[i].password)
+    {
+      console.log("Found a match")
+      return <Redirect to= "LoginSuccess"></Redirect>; 
+    }
+  }
+  if(i == len)
+  {
+    return <Redirect to= "LoginFail"></Redirect>; 
   }
 
-  // if(props.username == sampleuser && props.password == samplepassword)
-  // {
-  //   console.log("logging you in....."); 
-  //   return (<Redirect to = "/LoginSuccess"></Redirect>); 
-  // }
-  // else{
-  //   console.log("wrong username/password"); 
-  //   return(<Redirect to = "/LoginFail"></Redirect>); 
-  // }
+
 }
 
 function handleSubmit(event)
@@ -121,14 +65,23 @@ function handleSubmit(event)
   console.log("USERNAME IS " + event.target.username.value + " , PASSWORD IS " + event.target.password.value);
 }
 
+
 function App() {
   const[username, setUsername] = useState("stranger"); 
   const[password, setPassword] = useState(""); 
   const[submittedForm, setSubmittedForm] = useState(false); 
+  const[users, setUsers] = useState({
+    username: "",
+    password: ""
+  })
 
-  useEffect(() => {
-    console.log("username updated"); 
-  }); 
+  const updateUsers = () => {
+    return onSnapshot(collection(db, "users"), (snapshot) => 
+      setUsers(snapshot.docs.map((doc) => ({username: doc.data().username, password: doc.data().password}))
+      )
+    )
+  }
+  useEffect(updateUsers, []); //Runs only once
 
   return (
     <Router>
@@ -153,14 +106,14 @@ function App() {
       </Switch>
     </div>
     
-
-    {renderAuthenticate(username, password, submittedForm)}
+    {console.log(users)}
+    {console.log(users.length)}
+    {console.log(users[0])}
+    {console.log(users[1])}
+    {renderLoginResult(username, password, submittedForm, users)}
     </Router>
   );
 }
 
-export {
-  App, 
-  Authenticate
-}
+
 export default App; 
