fbc91246bb720c0b8af75249c984cced8957c23d
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2021-11-30 00:22:30 -0800

    Diet and intolerance options are stored in state variables.

--- src/components/DRItem.js
+++ src/components/DRItem.js
@@ -1,11 +1,21 @@
-/*
-
-*/
+import {useState} from "react";
+import React, { Component } from 'react';
 
 function DRItem(props) {
+    const [isChecked, changeChecked] = useState(false);
     return (
-        <div className = "DRItem">
-            <p>{props.itemName}</p>
+        <div>
+            <input type="checkbox" id={props.id} name={props.name} value={props.value} checked={isChecked} 
+                onChange={ () => {
+                    // console.log(isChecked);
+                    // if (!isChecked) {
+                    //     props.changeFunction(props.id, isChecked);
+                    // }
+                    props.changeFunction(props.id, isChecked);
+                    changeChecked(!isChecked); 
+                } }
+            />
+            <label for={props.id}>{props.value}</label>
         </div>
     );
 }
