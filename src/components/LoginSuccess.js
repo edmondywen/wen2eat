212aab85ea40bbff9153edaef7a031f147fac49e
Jess Xu <jsxu315@gmail.com>
2021-11-29 16:14:14 -0800

    minor changes

--- src/components/LoginSuccess.js
+++ src/components/LoginSuccess.js
@@ -4,7 +4,9 @@ export const LoginSuccess= ({}) => {
     return (
         <p>Login successful</p>
     )
-    //TODO: display the user's pantry (associate username with a collectionid probably)
+    //TODO: display the user's pantry 
+    //General idea: replace the body of this with the pantry/recommendations from the other branches, 
+    //except change the collection id to the user's specific collection id 
 }
 
 export default LoginSuccess;
